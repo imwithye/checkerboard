789c8762abdba5ba934834bdcfbb3a33ee057139
Ciel <imwithye@gmail.com>
2018-07-22 20:37:50 +0800

    load png metatable

--- lua_lodepng.c
+++ lua_lodepng.c
@@ -7,6 +7,38 @@
 
 #include "lodepng.h"
 
+#define PNG_METATABLE "lodepng.png"
+
+static int png_at(lua_State *L)
+{
+    lua_getfield(L, -1, "width");
+    const int width = luaL_checkinteger(L, -1);
+    lua_getfield(L, -2, "height");
+    const int height = luaL_checkinteger(L, -1);
+    printf("width = %d, height = %d\n", width, height);
+    return 0;
+}
+
+static int png_tostring(lua_State *L)
+{
+    lua_getfield(L, -1, "width");
+    const int width = luaL_checkinteger(L, -1);
+    lua_getfield(L, -2, "height");
+    const int height = luaL_checkinteger(L, -1);
+
+    char buffer[512];
+    sprintf(buffer, PNG_METATABLE"<width = %d, height = %d, data length = %d bytes>", width,
+            height, width * height * 4);
+    lua_pushstring(L, buffer);
+    return 1;
+}
+
+static const luaL_Reg png[] = {
+    {"at", png_at},
+    {"__tostring", png_tostring},
+    {NULL, NULL},
+};
+
 static int decode32(lua_State *L)
 {
     size_t pngsize;
@@ -34,6 +66,7 @@ static int decode32(lua_State *L)
         lua_pushliteral(L, "data");
         lua_pushlstring(L, (char *)image, width * height * 4);
         lua_settable(L, -3);
+        luaL_setmetatable(L, PNG_METATABLE);
         return 1;
     }
 }
@@ -46,5 +79,10 @@ static const luaL_Reg lodepng[] = {
 LUAMOD_API int luaopen_lodepng(lua_State *L)
 {
     luaL_newlib(L, lodepng);
+    luaL_newmetatable(L, PNG_METATABLE);
+    lua_pushvalue(L, -1);
+    lua_setfield(L, -2, "__index");
+    luaL_setfuncs(L, png, 0);
+    lua_pop(L, 1);
     return 1;
 }
--- test_lodepng.lua
+++ test_lodepng.lua
@@ -5,5 +5,4 @@ local file_data = file:read("*all")
 file:close()
 
 local png = lodepng.decode32(file_data)
-print(string.format("png width = %s, height = %s, data length = %d", png.width, png.height, string.len(png.data)))
 print(png)
